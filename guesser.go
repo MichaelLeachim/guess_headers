16c5ae7a15f8c62cfd17316ca9d7fb6c690c2417
mik <mklimoff222@gmail.com>
2019-02-18 19:45:13 +0200

    made cleanup function: TODO: test it

--- guesser.go
+++ guesser.go
@@ -6,11 +6,16 @@
 // @@@@@@ At 2019-02-18 19:09 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 package main
 
+import (
+	"strings"
+)
+
 // Guessing algorithm (ideas):
 // [Reduce space] Take random N(100) fields of each column of each file (reducing step)
 // [Tokenize] Tokenize numbers via splitting, i.e. 1924 => (1000 900 20 4)
 // [Match] Match columns: dot product with each other, keep only the best match
-// [Cleanup] Assume that there is only single one best match. If it is taken, there is nothing left (the match is nil)
+// [Cleanup] Assume that there is only single one best match.
+// If it is taken, there is nothing left (the match is nil)
 
 func MatchAlgoHamming(input1, input2 []string) float64 {
 
@@ -21,12 +26,23 @@ func CalculateBestMatch(input1 []string, input2 [][]string) Triplet {
 
 }
 
-// [Cleanup] Assume that there is only single one best match. If it is taken, there is nothing left (the match is nil)
+// [Cleanup] Assume that there is only single best match. If it is taken, there is nothing left (the match is nil)
 func CleanUp(data []Triplet) []Triplet {
-	for index,item :=data {
-		
+	scores := map[string]float64{}
+	for index, item := range data {
+		score, ok := scores[strings.Join(item.Right, "")]
+		if ok && score <= item.Score {
+			scores[strings.Join(item.Right, "")] = item.Score
+		}
 	}
-	
+	for index, item := range data {
+		score, ok := scores[strings.Join(item.Right, "")]
+		if ok && score <= item.Score {
+			data[index].Right = nil
+			data[index].Score = 0
+		}
+	}
+	return data
 }
 
 // returns match score
