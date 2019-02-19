87aaed85f05e473a56f25b2324b31a2e389183fb
mik <mklimoff222@gmail.com>
2019-02-19 19:40:06 +0200

    Added ReadXLSXFile function

---
+++ xlsx_transformer.go
@@ -0,0 +1,42 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @ Copyright (c) Michael Leahcim                                                      @
+// @ You can find additional information regarding licensing of this work in LICENSE.md @
+// @ You must not remove this notice, or any other, from this software.                 @
+// @ All rights reserved.                                                               @
+// @@@@@@ At 2019-02-19 19:31 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+
+package main
+
+import (
+	"errors"
+	"github.com/tealeg/xlsx"
+	"strings"
+)
+
+// !!! will work only on one sheet of the file !!!
+func WriteXLSXFile(fpath string, data [][]string) error {
+
+}
+
+// !!! will work only on one sheet of the file !!!
+func ReadXLSXFile(fpath string, sheetName string) ([][]string, error) {
+	result := [][]string{}
+	xlFile, err := xlsx.OpenFile(fpath)
+	if err != nil {
+		return result, err
+	}
+	for _, sheet := range xlFile.Sheets {
+		if strings.TrimSpace(sheet.Name) != strings.TrimSpace(sheetName) {
+			continue
+		}
+		for _, row := range sheet.Rows {
+			row4result := []string{}
+			for _, cell := range row.Cells {
+				row4result = append(row4result, cell.String())
+			}
+			result = append(result, row4result)
+		}
+		return result, nil
+	}
+	return result, errors.New("File: " + fpath + "does not contain sheet: " + sheetName)
+}
