1030565b3dc577b6dc4a7b92454207cda3eaa92c
mik <mklimoff222@gmail.com>
2019-02-19 19:47:29 +0200

    Added TestReadXLSXFile function

--- xlsx_transformer_test.go
+++ xlsx_transformer_test.go
@@ -6,3 +6,21 @@
 // @@@@@@ At 2019-02-19 19:44 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestReadXLSXFile(t *testing.T) {
+	testShouldEqual, _ := ReadCSVFile("testdata/input1.csv", ',')
+	result, err := ReadXLSXFile("testdata/input1.xlsx", "input1")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, testShouldEqual, result)
+}
+
+func TestWriteXLSXFile(t *testing.T) {
+
+}
