982833763c4845bdaae8fc1f953f388594c8299a
mik <mklimoff222@gmail.com>
2019-02-19 19:29:18 +0200

    Added Test for WriteCSVFile function

--- csv_transformer_test.go
+++ csv_transformer_test.go
@@ -9,14 +9,32 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestTakeSeed(t *testing.T) {
+func TestReadCSVFile(t *testing.T) {
 	csv, err := ReadCSVFile("testdata/input1.csv", ',')
 	assert.Equal(t, err, nil)
 	assert.Equal(t, csv[0], "")
 	transposedCsv := TransposeMatrix(csv)
 	assert.Equal(t, transposedCsv[0], "")
+}
 
+func TestWriteCSVFile(t *testing.T) {
+
+	tmpfile, err := ioutil.TempFile("", "tmp_csv_file")
+	assert.Equal(t, err, nil)
+	// remove after use
+	defer os.Remove(tmpfile.Name())
+
+	// read data for writing
+	csv, _ := ReadCSVFile("testdata/input1.csv", ',')
+	// write it into temp file
+	assert.Equal(t, WriteCSVFile(tmpfile.Name(), csv), nil)
+	// read again
+	csv2, err := ReadCSVFile(tmpfile.Name(), ',')
+	// assume equals
+	assert.Equal(t, csv, csv2)
 }
