3605868f2beee618895075853b88f45a51f7ef83
mik <thereisnodotcollective@gmail.com>
2019-02-19 20:03:50 +0200

    Added skeleton for scenarios: functions ReturnConcordHeaders & ReturnRightFileWithLeftHeaders & ReturnLeftFileWithRightHeaders & ReturnLeftJoinBetweenFiles

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 
 # TODO
 
+## [2019-02-19] [19:53]
+
+
 ## [2019-02-18] [21:10]
-* Make **guess** function
+* [DONE] Make **guess** function
 * Implement test for **guess** function
 * Implement test on small size real life data
 * 
-* Create transformer for CSV  format
+* [DONE] Create transformer for CSV  format
 * Create transformer for JSON format
-* Create transformer for XLSX format
+* [DONE] Create transformer for XLSX format
---
+++ scenarios.go
@@ -0,0 +1,28 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @ Copyright (c) Michael Leahcim                                                      @
+// @ You can find additional information regarding licensing of this work in LICENSE.md @
+// @ You must not remove this notice, or any other, from this software.                 @
+// @ All rights reserved.                                                               @
+// @@@@@@ At 2019-02-19 19:55 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+
+package main
+
+// will return concordance between right and left headers of the dataset
+func ReturnConcordHeaders(data [][]string) [][]string {
+
+}
+
+// Will return right file with left headers
+func ReturnRightFileWithLeftHeaders(data [][]string) [][]string {
+
+}
+
+// Will return left file with right headers
+func ReturnLeftFileWithRightHeaders(data [][]string) [][]string {
+
+}
+
+// will return left join between two files
+func ReturnLeftJoinBetweenFiles(data [][]string) [][]string {
+
+}
