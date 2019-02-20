9691a54063f2f82f1b155bcbb4ea4790ecfa3f59
mik <mklimoff222@gmail.com>
2019-02-20 20:04:46 +0200

    added test for TokenizeAlphaNumericOnly

--- tokenizers_test.go
+++ tokenizers_test.go
@@ -5,15 +5,18 @@
 // @ All rights reserved.                                                               @
 // @@@@@@ At 2019-02-20 19:52 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 
+package main
+
 import (
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
+
+func TestTokenizeAlphaNumericOnly(t *testing.T) {
+	assert.Equal(t, TokenizeAlphaNumericOnly("Привет hello !№;%:? world*"), "hello world")
+}
 
 func TestTokenizeNumbers(t *testing.T) {
 	assert.Equal(t, TokenizeNumbers("1920"), "1900 900 20 0")
 	assert.Equal(t, TokenizeNumbers("0.00345"), "0.003 0.0004 0.00005")
 }
-
-
